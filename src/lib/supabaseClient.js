f3c78523e7d2e1faf6d5c81851ec45549b6e1925
AnotherRizz <risqijapana55368@gmail.com>
2026-01-12 10:28:39 +0700

    move depedencies to env

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hawojxoxgwofsoejakrb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhhd29qeG94Z3dvZnNvZWpha3JiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwNDQ3NjcsImV4cCI6MjA2NTYyMDc2N30.-SHVByUscunxtmXygDdCEsKon236BWx1LmzTowaY80U';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
